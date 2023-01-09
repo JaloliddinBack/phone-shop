f66565f8f9e4ee9b73bcd0443b435fdd3afb9a6f
JaloliddinBack <jaloliddinrakhmatovvv3@gmail.com>
2023-01-09 14:55:53 +0500

    baza

---
+++ phone.js
@@ -0,0 +1,115 @@
+
+let products = [
+    {
+        "id": "1",
+        "name": "Iphone X",
+        "cost": "150",
+        "count": "12",
+        "category": "Apple",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Folcha.uz%2Fimage%2F360x360%2Fproducts%2FUCwIIkXzdWZum3TqBaeoUZTY6624kwDTBRGbV2a3Cgp5uAiAF89EhsKv2KTR.jpg&imgrefurl=https%3A%2F%2Folcha.uz%2Foz%2Fproduct%2Fview%2Fapple-iphone-x-64gb-silver&tbnid=Z6CfF6qyWgkJDM&vet=12ahUKEwjo7pu2lbr8AhVOS_UHHePfABsQMygHegUIARCyAQ..i&docid=vljThqhDQiuGtM&w=360&h=360&q=iphone%20x%20pictures&ved=2ahUKEwjo7pu2lbr8AhVOS_UHHePfABsQMygHegUIARCyAQ"
+    },
+    {
+        "id": "2",
+        "name": "Iphone XS max",
+        "cost": "179",
+        "count": "15",
+        "category": "Apple",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fopenshop.uz%2Fuploads%2Fproducts%2Fphotos%2Fz4IbC6xNpPZb5P0WzXHgwtFoxI0SakVUYHCv7x56.jpeg&imgrefurl=https%3A%2F%2Fopenshop.uz%2Fproduct%2F105709-iPhone-XS-Max-512Gb-Gray-Dual-zm3S7&tbnid=dsKfKSdw57ooIM&vet=12ahUKEwjbgMvxlbr8AhVRA6YKHZRyDngQMygAegUIARDAAQ..i&docid=z-tXLkl1NqPmEM&w=1000&h=1000&q=iphone%20xs%20max%20pictures&ved=2ahUKEwjbgMvxlbr8AhVRA6YKHZRyDngQMygAegUIARDAAQ"
+    },
+    {
+        "id": "3",
+        "name": "Iphone 11 pro max",
+        "cost": "189",
+        "count": "25",
+        "category": "Apple",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fopenshop.uz%2Fuploads%2Fproducts%2Fphotos%2F54qDILaXXgakES7J1GLanoDy1VfDbbOH.jpg&imgrefurl=https%3A%2F%2Fopenshop.uz%2Fshop%2Fproduct%2F165-apple-iphone-11-pro-max-64gb-green&tbnid=byk-0cw7GfGFMM&vet=12ahUKEwjk6-Owlrr8AhWWxosBHTu7CcgQMygAegUIARDDAQ..i&docid=YNqVyQwyCeXxxM&w=1000&h=1000&q=iphone%2011%20pro%20max%20pictures%20&ved=2ahUKEwjk6-Owlrr8AhWWxosBHTu7CcgQMygAegUIARDDAQ"
+    },
+    {
+        "id": "4",
+        "name": "Iphone 12 pro max",
+        "cost": "199",
+        "count": "28",
+        "category": "Apple",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fopenshop.uz%2Fuploads%2Fproducts%2Fphotos%2FHyLPChJYd4nYgHAtdSY3clQNWEdnewU2Z0YofdlV.jpeg&imgrefurl=https%3A%2F%2Fopenshop.uz%2Fshop%2Fproduct%2F14572-smartfon-apple-iphone-12-pro-max-128gb-blue&tbnid=BURuldAkbQTX8M&vet=12ahUKEwjbq4fVlrr8AhVlLqYKHQv6CqYQMygAegUIARC8AQ..i&docid=0pcYridD1sE6uM&w=850&h=850&q=iphone%2012%20pro%20max%20pictures&ved=2ahUKEwjbq4fVlrr8AhVlLqYKHQv6CqYQMygAegUIARC8AQ"
+    },
+    {
+        "id": "5",
+        "name": "Iphone 13 pro max",
+        "cost": "200",
+        "count": "45",
+        "category": "Apple",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fzoodmall.com%2Fcdn-cgi%2Fimage%2Fw%3D600%2Cfit%3Dcontain%2Cf%3Dauto%2Fhttps%3A%2F%2Fimages.zoodmall.com%2Fcloudinary%2Fc7%2F0b4%2Fhttps%25253A%25252F%25252Favatars.mds.yandex.net%25252Fget-mpic%25252F5235448%25252Fimg_id4222516894328537616.jpeg%25252Forig&imgrefurl=https%3A%2F%2Fwww.zoodmall.uz%2Fru%2Fproduct%2F20339700%2Fsmartfon-apple-iphone-13-pro-max-6128gb-global-graphite%2F&tbnid=HgthPadxMZjo3M&vet=12ahUKEwinkZyZl7r8AhW2RvUHHb9-Cg8QMygHegUIARDOAQ..i&docid=3f3nsrbo7tJjdM&w=600&h=762&q=iphone%2013%20pro%20max%20pictures&ved=2ahUKEwinkZyZl7r8AhW2RvUHHb9-Cg8QMygHegUIARDOAQ"
+    },
+    {
+        "id": "6",
+        "name": "Iphone 14 pro max",
+        "cost": "149",
+        "count": "49",
+        "category": "Apple",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.macbro.uz%2Fmacbro%2F13af4b35-e0e8-4fd0-8e0e-4b690860dfb3&imgrefurl=https%3A%2F%2Fmacbro.uz%2Fproduct%2Fiphone-14-pro-max&tbnid=tNY2xw4-d5X6uM&vet=12ahUKEwissv64l7r8AhVTC94KHeP-BgwQMygAegUIARCIAg..i&docid=PT8ayGXQBaZGgM&w=594&h=725&q=iphone%2014%20pro%20max%20pictures&ved=2ahUKEwissv64l7r8AhVTC94KHeP-BgwQMygAegUIARCIAg"
+    },
+    {
+        "id": "7",
+        "name": "Airpods pro",
+        "cost": "99",
+        "count": "56",
+        "category": "Airpods Naushnik",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fopenshop.uz%2Fuploads%2Fproducts%2Fphotos%2FwX6qkJzjoeA9d0xeiFCYCStymyzgDUat.jpg&imgrefurl=https%3A%2F%2Fopenshop.uz%2Fshop%2Fproduct%2F912-naushniki-apple-airpods-pro&tbnid=5n80BhsOBXXNAM&vet=12ahUKEwiNnbDOl7r8AhUQgpQKHcNFD60QMygAegUIARC6AQ..i&docid=WIUNNr3aQsarrM&w=1000&h=1000&q=airpods%20pro&ved=2ahUKEwiNnbDOl7r8AhUQgpQKHcNFD60QMygAegUIARC6AQ"
+    },
+    {
+        "id": "8",
+        "name": "Airpods pro 2",
+        "cost": "209",
+        "count": "5",
+        "category": "Airpods Naushnik",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fjabko.ua%2Fimage%2Fcache%2Fcatalog%2Fproducts%2F2022%2F09%2F072342%2FMQD83%2520(1)-1397x1397.jpg&imgrefurl=https%3A%2F%2Fjabko.ua%2Frus%2Fapple-airpods%2Fnaushniki-apple-airpods%2Fnaushniki-apple-airpods-pro-2-with-magsafe-charging-case-2022&tbnid=OEWemybViqULrM&vet=12ahUKEwjR0OOwmLr8AhVdTPUHHe4eC0kQMygQegUIARDfAQ..i&docid=-EeOqmXn4PTy_M&w=1167&h=1167&q=airpods%20pro%202&ved=2ahUKEwjR0OOwmLr8AhVdTPUHHe4eC0kQMygQegUIARDfAQ"
+    },
+    {
+        "id": "9",
+        "name": "Airpods pro 3",
+        "cost": "149",
+        "count": "11",
+        "category": "Airpods Naushnik",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.macbro.uz%2Fmacbro%2F038df3d3-5514-48d1-b09f-4a2f7fca2de8&imgrefurl=https%3A%2F%2Fmacbro.uz%2Fproduct%2Fairpods-3-pglydpkv7sir14&tbnid=AGchb-wf3cgR3M&vet=12ahUKEwjc77bUmLr8AhWUSfUHHaevB98QMygDegUIARDAAQ..i&docid=RzBLzZ5IPqGtFM&w=1080&h=1080&q=airpods%20pro%203&ved=2ahUKEwjc77bUmLr8AhWUSfUHHaevB98QMygDegUIARDAAQ"
+    },
+    {
+        "id": "10",
+        "name": "Airpods pro 4",
+        "cost": "169",
+        "count": "14",
+        "category": "Airpods Naushnik",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Ffrankfurt.apollo.olxcdn.com%2Fv1%2Ffiles%2Fw8l1nwwnu0m51-UZ%2Fimage%3Bs%3D1080x1080&imgrefurl=https%3A%2F%2Fwww.olx.uz%2Fd%2Fobyavlenie%2Fbepul-dostavka-novye-airpods-pro-4-zapechatannye-pro-4-shoshiling-ID2m1wR.html&tbnid=coWNIWffN-qukM&vet=12ahUKEwjohpP5mLr8AhWtTPUHHS36Cw0QMygCegUIARC-AQ..i&docid=VY1BBrHsr-YLgM&w=1080&h=1080&q=airpods%20pro%204&ved=2ahUKEwjohpP5mLr8AhWtTPUHHS36Cw0QMygCegUIARC-AQ"
+    },
+    {
+        "id": "11",
+        "name": "Aplle naushnik",
+        "cost": "249",
+        "count": "54",
+        "category": "Naushnik",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fhotline.ua%2Fimg%2Ftx%2F240%2F2406926795.jpg&imgrefurl=https%3A%2F%2Fhotline.ua%2Fav-naushniki-garnitury%2Fapple-earpods-with-remote-and-mic-md827%2F&tbnid=HzSFGPayBzBqrM&vet=12ahUKEwiOhu-Umbr8AhXDc3AKHZKRDR8QMygeegUIARD9AQ..i&docid=oFuHRlAtsebH1M&w=1820&h=1820&q=apple%20%D0%BD%D0%B0%D1%83%D1%88%D0%BD%D0%B8%D0%BA&ved=2ahUKEwiOhu-Umbr8AhXDc3AKHZKRDR8QMygeegUIARD9AQ"
+    },
+    {
+        "id": "12",
+        "name": "Naushnik",
+        "cost": "239",
+        "count": "7",
+        "category": "Naushnik",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.eldorado.ru%2Fphotos%2F71%2F712%2F204%2F95%2Fnew_71220495_l_1479819252.jpeg&imgrefurl=https%3A%2F%2Fwww.eldorado.ru%2Fcat%2Fdetail%2Fnaushniki-s-mikrofonom-apple-earpods-with-lightning-connector-mmtn2zm-a%2F&tbnid=1G3857KYmcoC2M&vet=12ahUKEwiOhu-Umbr8AhXDc3AKHZKRDR8QMygpegUIARCVAg..i&docid=YnzIiIAS8FnbvM&w=281&h=500&q=apple%20%D0%BD%D0%B0%D1%83%D1%88%D0%BD%D0%B8%D0%BA&ved=2ahUKEwiOhu-Umbr8AhXDc3AKHZKRDR8QMygpegUIARCVAg"
+    },
+    {
+        "id": "13",
+        "name": "Quloqchin",
+        "cost": "349",
+        "count": "96",
+        "category": "Naushnik",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fallgood.uz%2Fstorage%2Fproducts%2FDecember2021%2Fyoeq0iUrqumAkhPDSZVI.png&imgrefurl=https%3A%2F%2Fallgood.uz%2Fproduct%2F4653-airpods-max-kosmiceskii-seryi&tbnid=qyCaHpc5W9D81M&vet=12ahUKEwiOhu-Umbr8AhXDc3AKHZKRDR8QMygBegUIARC3AQ..i&docid=nQOTVoYN72FmOM&w=900&h=900&q=apple%20%D0%BD%D0%B0%D1%83%D1%88%D0%BD%D0%B8%D0%BA&ved=2ahUKEwiOhu-Umbr8AhXDc3AKHZKRDR8QMygBegUIARC3AQ"
+    },
+    {
+        "id": "14",
+        "name": "Simli naushnik",
+        "cost": "369",
+        "count": "89",
+        "category": "Naushnik",
+        "img_src": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fairpodsi.ru%2Fwp-content%2Fuploads%2Foriginalnye-naushniki-apple-fd70821.jpg&imgrefurl=https%3A%2F%2Fairpodsi.ru%2Foriginalnye-nayshniki-apple%2F&tbnid=kPCpn8jW-EcI_M&vet=10CMEBEDMovwFqFwoTCNiD5KCZuvwCFQAAAAAdAAAAABAD..i&docid=v2pRd2Ryc9j4tM&w=450&h=388&q=apple%20%D0%BD%D0%B0%D1%83%D1%88%D0%BD%D0%B8%D0%BA&ved=0CMEBEDMovwFqFwoTCNiD5KCZuvwCFQAAAAAdAAAAABAD"
+    }
+]
